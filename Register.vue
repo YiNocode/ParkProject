8d8c644c9f04b9a662a90864d3e51c21e72d44e2
YiNocode <98638400+YiNocode@users.noreply.github.com>
2022-02-02 15:26:42 +0800

    Update Register.vue

--- Register.vue
+++ Register.vue
@@ -1,17 +1,12 @@
 <template>
-	<view>
-		<uni-forms ref="form" :modelValue="formData" :rules="rules">
-			<uni-forms-item name="username">
-				<input  v-model="formData.username" placeholder="请输入用户名"  />
-			</uni-forms-item>
-			<uni-forms-item name="password">
-				<input  v-model="formData.password" placeholder="请输入密码" password=true/>
-			</uni-forms-item>
-			<uni-forms-item name="passwordConfirm">
-				<input  v-model="formData.passwordConfirm" placeholder="确认密码" password=true />
-			</uni-forms-item>
-		</uni-forms>
-		<button @click="submit">注册</button>
+	<view class="screen">
+		
+		<form @submit="formSubmit">
+			<input class="input" name="username" placeholder="请输入用户名" />
+			<input class="input" name="password" placeholder="请输入密码" password=true/>
+			<button class="button" form-type="submit" >登录</button>
+		</form>
+		<navigator class="button"  url="../Register/Register"><button>注册</button></navigator>
 	</view>
 </template>
 
@@ -21,113 +16,121 @@
 			return {
 				token:'',
 				isSuccess:false,
-				formData:{
-					username:'',
-					password:'',
-					passwordConfirm:'',
-				},
-				rules:{
-					username:{
-						rules:[
-							{
-								required:true,
-								errorMessage:"请输入用户名",
-								
-							},
-							{
-								maxLength:8,
-								errorMessage:"用户名不能超过8个字符",
-								
-							},
-							{
-								minLength:3,
-								errorMessage:"用户名不应少于3个字符"
-							}
-						]
-					},
-					password:{
-						rules:[
-							{
-								required:true,
-								errorMessage:"请输入密码",
-							},
-							{
-								minLength:6,
-								errorMessage:"密码应长于6个字符",
-							},
-							{
-								maxLength:15,
-								errorMessage:"密码不应长于15个字符"
-							},
-						]
-					},
-					passwordConfirm:{
-						rules:[
-							{
-								required:true,
-								errorMessage:"请确认密码",
-							},
-							{
-								validateFunction:function(rule,value,data,callback){
-									if(value!=data.password)
-									{
-										callback('两次密码输入不一致')
-									}
-									return true
-								}
-							}
-						]
-						
-					}
-				}
+				latitude:0,
+				longitude:0,
 				
 			}
 		},
 		onReady() {
-			this.$refs.form.setRules(this.rules)
+			uni.getStorage({
+				key:'userInfo',
+				success:function(res)
+				{
+					uni.showToast({
+						title:'您已登录',
+						
+					})
+				}
+			})
 		},
 		methods: {
-			submit:function(){
-				this.$refs.form.validate().then(res=>{
-					uni.request({
-						url:'http://47.97.90.35:8080/register',
-						data:JSON.stringify(res),
-						method:'POST',
-						success(res) {
-							if(res.data.code=="200")
-							{
-								uni.showToast({
-									title:'注册成功',
-								});
-								this.token=res.data.data.token;
-								this.isSuccess=true;
-							}
-							else{
-								uni.showToast({
-									title:'用户已存在',
-									icon:'error'
-								});
-							}
-							
-						},
-						fail(){
+			formSubmit:function(e){
+				console.log("ok");
+				uni.request({
+					url:'http://47.97.90.35:8080/login',
+					method:'POST',
+					data:JSON.stringify(e.detail.value),
+					success: res => {
+						if(res.data.code=="200"){
 							uni.showToast({
-								title:'网络错误，请重试',
-								icon:'none'
+								title: '登录成功'
+							});
+							this.token=res.data.token;
+							uni.setStorage({
+								key:'userInfo',
+								data:JSON.stringify(e.detail.value.username),
+								complete(data) {
+									console.log('complete')
+									console.log(data)
+								}
+								
+							})
+							console.log(res)
+							uni.setStorage({
+								key:'token',
+								data:JSON.stringify(res.data.data.token)
+								
+							})
+							this.isSuccess=true;
+							uni.getLocation({
+								success(res) {
+									uni.redirectTo({
+										url:'../Map/Map?data='+JSON.stringify({latitude:res.latitude,longitude:res.longitude}) ,
+										complete() {
+											console.log(res.latitude)
+											console.log(res.longitude)
+										}
+									})
+								
+								}
+							})	
+						}
+						else{
+							uni.showToast({
+								title: '请检查用户名或密码',
+								icon:"none"
 							});
 						}
-					})
-				}).catch(err=>{
-					console.log(err);
+					},
+					fail() {
+						uni.showToast({
+							title:"网络错误，请检查网络"
+						})
+					}
 				})
-				
-				
-			}
-			
+			},
 		}
 	}
 </script>
 
 <style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
 
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin-top: 200rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50rpx;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	}
+	.input{
+		margin: 70rpx;
+	
+	}
+	
+	.screen{
+			
+	}
+	.button{
+		margin: 50rpx;
+		font:'Gill Sans', 
+		
+	}
+	
 </style>
